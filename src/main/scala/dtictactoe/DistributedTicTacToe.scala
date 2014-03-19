3d040bae0a6911dc66356e1b73b12527da365f5a
Timothy Hahn <timyhahn@gmail.com>
2014-03-18 21:02:03 -0400

    Implemented a Dumb Actor

--- src/main/scala/dtictactoe/DistributedTicTacToe.scala
+++ src/main/scala/dtictactoe/DistributedTicTacToe.scala
@@ -12,9 +12,12 @@ import scala.concurrent.duration._
 object DistributedTicTacToe extends App {
   implicit val timeout = Timeout(1 seconds)
 
+
+  val emptyBoard: List[List[Char]] = List.fill[Char](3,3) { ' ' }
+
   val actorSystem = ActorSystem()
   val firstActor = actorSystem.actorOf(Props[DumbActor])
-  val future = firstActor ? new Board(List(List[Char]()))
+  val future = firstActor ? new Board(emptyBoard)
   val result = Await.result(future, timeout.duration).asInstanceOf[(Int, Int)]
   println(s"Board Received! $result")
   actorSystem.shutdown
--- src/main/scala/dtictactoe/TicTacToeActor.scala
+++ src/main/scala/dtictactoe/TicTacToeActor.scala
@@ -6,13 +6,22 @@ import akka.actor.Actor
 case class Board(board: List[List[Char]])
 
 class DumbActor extends TicTacToeActor {
-  def calculateMove(board: List[List[Char]]): (Int, Int) = {
-    (1, 1)
-  }
 
+  def randomMove(moves: List[(Int, Int)]): (Int, Int) = moves(util.Random.nextInt(moves.length)) 
+
+  def calculateMove(board: List[List[Char]]): (Int, Int) = randomMove(validMoves(board))
 }
 
 trait TicTacToeActor extends Actor {
+
+  def validMoves(board: List[List[Char]]): List[(Int, Int)] = {
+    (for {
+      x <- 0 to 2
+      y <- 0 to 2
+      if(board(x)(y) == ' ')
+    } yield (x, y)).toList
+  }
+
   def calculateMove(board: List[List[Char]]): (Int, Int)
   def receive = {
     case board: Board =>
