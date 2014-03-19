a3f60b50560ee17524f22635bd397bd04bc7480b
Timothy Hahn <timyhahn@gmail.com>
2014-03-19 00:17:03 -0400

    Removed mutability

--- src/main/scala/dtictactoe/DistributedTicTacToe.scala
+++ src/main/scala/dtictactoe/DistributedTicTacToe.scala
@@ -11,27 +11,28 @@ import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
 
 object DistributedTicTacToe extends App {
-  implicit val timeout = Timeout(1 seconds)
+  implicit val timeout = Timeout(10 seconds)
 
   val actorSystem = ActorSystem()
+  val playerCount = 50
+  val gameCount = 10
 
-  var board = TicTacToe.emptyBoard
   val players = for {
-    x <- 1 to 50
+    x <- 1 to playerCount
   } yield TicTacToe.uuid
 
   players.foreach(player => actorSystem.actorOf(Props(new DumbActor), player))
 
   val results = for {
-    x <- 1 to 10
+    x <- 1 to gameCount
     roster = TicTacToe.rng.shuffle(players)
     matchups = roster.take(players.length / 2) zip roster.takeRight(players.length / 2)
     games = matchups.map(matchup => (TicTacToe.uuid, matchup))
     results = games.map{game => 
-      actorSystem.actorOf(Props(new GameActor)) ? new Game(game._1, game._2)
+      actorSystem.actorOf(Props(new GameActor)) ? new Game(game._2)
     }
     futureResults = Future.sequence(results)
-  } yield Await.result(futureResults, 5 seconds)
+  } yield Await.result(futureResults, timeout.duration)
 
   println(results.flatten.groupBy(x => x).map(a => (a._1, a._2.length)).toSeq.sortWith(_._2 > _._2))
 
--- src/main/scala/dtictactoe/GameActor.scala
+++ src/main/scala/dtictactoe/GameActor.scala
@@ -10,42 +10,40 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 
-case class Game(uuid: String, matchup: (String, String))
+case class Game(matchup: (String, String))
 
 class GameActor extends Actor {
-  implicit val timeout = Timeout(1 seconds)
-
-  def receive = {
-    case game: Game =>
-      val matchup = game.matchup
-      var board = TicTacToe.emptyBoard
-      var xTurn = true 
-
-      while(!TicTacToe.isGameOver(board)) {
-        val actorName = xTurn match{
-          case true => matchup._1
-          case false => matchup._2
-        }
-        val actorSelection = context.system.actorSelection(s"user/$actorName")
-        val player = xTurn match {
-          case true => 'x'
-          case false => 'o'
-        }
-
-        val future = actorSelection ? new Board(board)
-        val move = Await.result(future, timeout.duration).asInstanceOf[(Int, Int)]
-        board = TicTacToe.applyMove(board, move, player)
-        xTurn = !xTurn
-      }
-
-      sender ! ((TicTacToe.isWonBy(board, 'x'), TicTacToe.isWonBy(board, 'o')) match {
+  implicit val timeout = Timeout(2 seconds)
+  def playGame(matchup: (String, String), board: List[List[Char]], xTurn: Boolean): String = {
+    if(TicTacToe.isGameOver(board)) {
+      ((TicTacToe.isWonBy(board, 'x'), TicTacToe.isWonBy(board, 'o')) match {
         case(true, _) =>
           matchup._1
         case(_, true) =>
           matchup._2
         case _ =>
           "DRAW"
-      })
-  }
+      }) 
+    } else {
+      val actorName = xTurn match {
+        case true => matchup._1
+        case false => matchup._2
+      }
+      val actorSelection = context.system.actorSelection(s"user/$actorName")
+      val player = xTurn match {
+        case true => 'x'
+        case false => 'o'
+      }
 
+      val future = actorSelection ? new Board(board)
+      val move = Await.result(future, timeout.duration).asInstanceOf[(Int, Int)]
+      playGame(matchup, TicTacToe.applyMove(board, move, player), !xTurn)
+    }
+  }
+  
+
+  def receive = {
+    case Game(matchup: (String, String)) =>
+      sender ! playGame(matchup, TicTacToe.emptyBoard, true)
+  }
 }
